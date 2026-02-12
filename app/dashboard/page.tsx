3792f823a740dd1b449fca274ff9e0dbd6b1af4d
ahmed ksontini <57683675+ahmedksont@users.noreply.github.com>
2026-02-12 18:57:27 +0100

    d

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
 
   return (
     <>
-      <Header />
+  
 
       {user.role === 'ETUDIANT' && <StudentDashboard />}
       {user.role === 'MEDECIN' && <DoctorDashboard />}
