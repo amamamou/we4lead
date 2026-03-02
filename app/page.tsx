a1cb4d70031b07c9165f29dd05e2de3d3547fc0b
amamamou <amamoumaram88@gmail.com>
2026-03-02 02:57:15 +0100

    delete unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
 import LandingHeader from '../components/landing/landing-header';
-import LandingInstitutions from '../components/landing/landing-institutions';
 import LandingCTA from '../components/landing/landing-cta';
 import   Footer  from '../components/landing/landing-footer';
-import PartnersSection from '@/components/landing/partners-section';
 import  TherapistsSection  from '../components/landing/psychotherapists-section';
-import GuidedReportSection from '@/components/landing/GuidedReportSection';
 import { HowItWorksSection } from '@/components/landing/how-it-works';
 import { FAQSection } from '@/components/landing/faq-section';
-import { CaseStudiesCarousel } from '@/components/landing/CaseStudiesCarousel';
-import { ProductTeaserCard } from '@/components/landing/BankingScaleHelo';
 import HeroSection from '@/components/landing/landing-hero';
-import { SocialProof } from '@/components/landing/social-proof';
 import { BentoGrid } from '@/components/landing/bento-grid';
 
 export default function Home() {
